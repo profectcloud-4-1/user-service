de8dd080e8382906b1aaca789de4d23a741c3edc
archive-dev-master <onlybussinessmail@gmail.com>
2025-10-29 16:35:22 +0900

    docs: Orde Swagger 명세 수정(#55)

--- src/main/java/profect/group1/goormdotcom/order/controller/external/v1/OrderController.java
+++ src/main/java/profect/group1/goormdotcom/order/controller/external/v1/OrderController.java
@@ -7,7 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import profect.group1.goormdotcom.apiPayload.ApiResponse;
 import profect.group1.goormdotcom.order.controller.external.v1.dto.OrderRequestDto;
 import profect.group1.goormdotcom.order.service.OrderService;
 import profect.group1.goormdotcom.order.domain.Order;
@@ -16,39 +16,39 @@ import profect.group1.goormdotcom.user.controller.auth.LoginUser;
 @RestController
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
-public class OrderController {
+public class OrderController implements OrderApiDocs{
 
     private final OrderService orderService;
     
     //주문생성 
     // *POST /api/v1/orders
     @PostMapping
-    public ResponseEntity<Order> create(@Valid @RequestBody OrderRequestDto req, @LoginUser UUID userId) {
+    public ApiResponse<Order> create(@Valid @RequestBody OrderRequestDto req, @LoginUser UUID userId) {
         Order order = orderService.create(userId,req);
-        return ResponseEntity.ok(order);
+        return ApiResponse.onSuccess(order);
     }
     
     //배송 전 취소 (환불)
     @PostMapping("/{orderId}/cancel/delivery-before")
-    public ResponseEntity<Order> deliveryBefore(@PathVariable UUID orderId) {
-        return ResponseEntity.ok(orderService.delieveryBefore(orderId));
+    public ApiResponse<Order> deliveryBefore(@PathVariable UUID orderId) {
+        return ApiResponse.onSuccess(orderService.delieveryBefore(orderId));
     }
 
     //배송 후 취소 (반송)
     @PostMapping("/{orderId}/cancel/return")
-    public ResponseEntity<Order> cancel(@PathVariable UUID orderId) {
-        return ResponseEntity.ok(orderService.cancel(orderId));
+    public ApiResponse<Order> cancel(@PathVariable UUID orderId) {
+        return ApiResponse.onSuccess(orderService.cancel(orderId));
     }
  
     //전체 조회
     @GetMapping
-    public ResponseEntity<List<Order>> getAllOrders(){
-        return ResponseEntity.ok(orderService.getAll());
+    public ApiResponse<List<Order>> getAllOrders(){
+        return ApiResponse.onSuccess(orderService.getAll());
     }
 
     //단건 조회
     @GetMapping("/{id}")
-    public ResponseEntity<Order> getOne(@PathVariable UUID id){
-        return ResponseEntity.ok(orderService.getOne(id));
+    public ApiResponse<Order> getOne(@PathVariable UUID id){
+        return ApiResponse.onSuccess(orderService.getOne(id));
     }
 }
