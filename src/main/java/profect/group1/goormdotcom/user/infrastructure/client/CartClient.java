7b8455ec68bbedbc18f97ced05fc9bf84fa37c61
ThinkKat <jwy06048@gmail.com>
2025-11-05 16:25:09 +0900

    :wrench: [chore] add the docker compose file

--- src/main/java/profect/group1/goormdotcom/user/infrastructure/client/CartClient.java
+++ src/main/java/profect/group1/goormdotcom/user/infrastructure/client/CartClient.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpHeaders;
 import profect.group1.goormdotcom.apiPayload.ApiResponse;
 
 @FeignClient(
-		name = "${spring.cloud.client.config.user-to-cart.name}",
+		name = "${spring.cloud.openfeign.client.config.user-to-cart.name}",
 		fallback = CartClientFallBack.class
 )
 public interface CartClient {
