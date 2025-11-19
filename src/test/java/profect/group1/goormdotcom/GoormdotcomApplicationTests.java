75e8ba0cdd6468021a49ef28781c0d2dd81f5a46
ohu-star <audtj0703@naver.com>
2025-11-19 16:23:33 +0900

    ✅ [test]#37 test profile 추가

--- src/test/java/profect/group1/goormdotcom/GoormdotcomApplicationTests.java
+++ src/test/java/profect/group1/goormdotcom/GoormdotcomApplicationTests.java
@@ -1,10 +1,14 @@
 package profect.group1.goormdotcom;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import profect.group1.goormdotcom.common.security.JwtTokenProvider;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -12,8 +16,14 @@ class GoormdotcomApplicationTests {
     @MockitoBean
     JwtTokenProvider jwtTokenProvider;
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private Environment environment;
+
+    @Test
+    void contextLoads() {
+        System.out.println("==== ACTIVE PROFILES ====");
+        System.out.println(Arrays.toString(environment.getActiveProfiles()));
+        System.out.println("==========================");
+    }
 
 }
