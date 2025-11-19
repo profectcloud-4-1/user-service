8b1660b0c78e2ec399d3a0267cca49ffc79094b8
ohu-star <audtj0703@naver.com>
2025-11-19 15:47:32 +0900

    🔨 [fix]#37 TokenProvider Mock처리

--- src/test/java/profect/group1/goormdotcom/GoormdotcomApplicationTests.java
+++ src/test/java/profect/group1/goormdotcom/GoormdotcomApplicationTests.java
@@ -3,10 +3,14 @@ package profect.group1.goormdotcom;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import profect.group1.goormdotcom.common.security.JwtTokenProvider;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class GoormdotcomApplicationTests {
+    @MockitoBean
+    JwtTokenProvider jwtTokenProvider;
 
 	@Test
 	void contextLoads() {
